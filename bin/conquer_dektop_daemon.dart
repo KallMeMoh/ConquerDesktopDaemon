cfdaab455f065fceaf1ebd4e9ad0d7da49ebfd6b
KallMeMoh <kallmemoh@gmail.com>
2026-04-17 18:07:19 +0200

    first commit

---
+++ bin/conquer_dektop_daemon.dart
@@ -0,0 +1,5 @@
+import 'package:conquer_dektop_daemon/conquer_dektop_daemon.dart' as conquer_dektop_daemon;
+
+void main(List<String> arguments) {
+  print('Hello world: ${conquer_dektop_daemon.calculate()}!');
+}
---
+++ test/conquer_dektop_daemon_test.dart
@@ -0,0 +1,8 @@
+import 'package:conquer_dektop_daemon/conquer_dektop_daemon.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
