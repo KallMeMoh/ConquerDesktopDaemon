0c509aa2846156125833587f75416d8f071a990d
KallMeMoh <kallmemoh@gmail.com>
2026-04-17 21:24:04 +0200

    Init logger

--- bin/conquer_dektop_daemon.dart
+++ bin/conquer_dektop_daemon.dart
@@ -1,5 +1,31 @@
-import 'package:conquer_dektop_daemon/conquer_dektop_daemon.dart' as conquer_dektop_daemon;
+import 'dart:io';
+
+import 'package:logging/logging.dart';
+import 'package:path/path.dart';
 
 void main(List<String> arguments) {
-  print('Hello world: ${conquer_dektop_daemon.calculate()}!');
+  String folderPath;
+
+  if (Platform.isWindows) {
+    folderPath = join(Platform.environment['APPDATA']!, 'my_daemon');
+  } else {
+    folderPath = join(
+      Platform.environment['HOME']!,
+      '.local',
+      'share',
+      'my_daemon',
+    );
+  }
+
+  Directory(folderPath).createSync(recursive: true);
+
+  final logPath = join(folderPath, 'log_file.log');
+
+  Logger.root.onRecord.listen((record) {
+    final file = File(logPath);
+    file.writeAsStringSync(
+      '${record.time}: [${record.level.name}] ${record.message}\n',
+      mode: FileMode.append,
+    );
+  });
 }
