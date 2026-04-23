c317ef51478f65e09b2b5c361725d66abc834396
KallMeMoh <kallmemoh@gmail.com>
2026-04-24 01:29:54 +0300

    remove unused dependancies

--- lib/notification_service.dart
+++ lib/notification_service.dart
@@ -1,9 +1,6 @@
 import 'dart:io';
-import 'dart:ffi';
 
 import 'package:dbus/dbus.dart';
-import 'package:win32/win32.dart';
-import 'package:ffi/ffi.dart';
 
 class NotificationService {
   static Future<void> show(String title, String content) async {
@@ -15,31 +12,33 @@ class NotificationService {
   }
 
   static void _showWindowsNotification(String title, String content) {
-    final script = """
-    \$ErrorActionPreference = 'Stop'
-    [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType=WindowsRuntime] | Out-Null
-    \$template = [Windows.UI.Notifications.ToastTemplateType]::ToastText02
-    \$xml = [Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent(\$template)
-    \$nodes = \$xml.GetElementsByTagName('text')
-    \$titleNode = \$nodes.Item(0)
-    \$messageNode = \$nodes.Item(1)
-    \$titleNode.AppendChild(\$xml.CreateTextNode('$title')) | Out-Null
-    \$messageNode.AppendChild(\$xml.CreateTextNode('$content')) | Out-Null
-    \$toast = [Windows.UI.Notifications.ToastNotification]::new(\$xml)
-    \$notifier = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier('Conquer')
-    \$notifier.Show(\$toast)
-    """;
+    final script =
+        """
+          \$ErrorActionPreference = 'Stop'
+          [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType=WindowsRuntime] | Out-Null
+          \$template = [Windows.UI.Notifications.ToastTemplateType]::ToastText02
+          \$xml = [Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent(\$template)
+          \$nodes = \$xml.GetElementsByTagName('text')
+          \$titleNode = \$nodes.Item(0)
+          \$messageNode = \$nodes.Item(1)
+          \$titleNode.AppendChild(\$xml.CreateTextNode('$title')) | Out-Null
+          \$messageNode.AppendChild(\$xml.CreateTextNode('$content')) | Out-Null
+          \$toast = [Windows.UI.Notifications.ToastNotification]::new(\$xml)
+          \$notifier = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier('Conquer')
+          \$notifier.Show(\$toast)
+        """;
 
-    final result = Process.runSync(
-      'powershell',
-      ['-NoProfile', '-NonInteractive', '-Command', script],
-    );
+    final result = Process.runSync('powershell', [
+      '-NoProfile',
+      '-NonInteractive',
+      '-Command',
+      script,
+    ]);
 
     if (result.exitCode != 0) {
       print('Error: ${result.stderr}');
     }
   }
-
 
   static Future<void> _showLinuxNotification(
     String title,
